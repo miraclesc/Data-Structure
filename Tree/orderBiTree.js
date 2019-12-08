3ac50f58134f01b7c505b97d6a4cd71573f72d7c
Shi Chong <xmu_marvel@qq.com>
2019-12-08 20:40:14 +0800

    Update orderBiTree.js

--- Tree/orderBiTree.js
+++ Tree/orderBiTree.js
@@ -4,7 +4,26 @@ function TreeNode(val) {
   this.left = null;
   this.right = null;
 }
+
 //先序遍历
+var preorderTraversal = function(root) {
+    var node = [];
+    var stack = [], top =0;
+    stack.push(root)
+    while(top!=-1){
+        while(stack[top]){
+            node.push(stack[top].val);
+            stack.push(stack[top].left);
+            top++;
+        }
+        stack.pop();
+        top--;
+        if(top!=-1){
+            stack.push(stack.pop().right);
+        }
+    }
+    return node;
+};
 
 //中序遍历
 var inorderTraversal = function(root) {
