e999065d8c4c1fb4af02865417aa01ac351d5ce2
Shi Chong <xmu_marvel@qq.com>
2019-12-09 22:13:56 +0800

    Create buildTree.js

---
+++ Tree/buildTree.js
@@ -0,0 +1,23 @@
+//前序与中序遍历序列构造二叉树 假设树中没有重复的元素
+
+function TreeNode(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+}
+
+var buildTree = function(preorder, inorder) {
+    var pre_index = 0;
+    var helper = function(in_left, in_right){
+        if(in_left == in_right){
+            return null;
+        }
+        var target = preorder[pre_index++];
+        var root = new TreeNode(target);
+        var index = inorder.indexOf(target);
+        root.left = helper(in_left, index);
+        root.right = helper(index+1, in_right);
+        return root;
+    }
+    return helper(0, inorder.length);
+};
