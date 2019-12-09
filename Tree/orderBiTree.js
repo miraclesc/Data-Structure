b97bea9c858485490e1600224c5a348937721a8f
Shi Chong <xmu_marvel@qq.com>
2019-12-09 19:45:00 +0800

    Update orderBiTree.js

--- Tree/orderBiTree.js
+++ Tree/orderBiTree.js
@@ -1,4 +1,4 @@
-//先序、中序、后序非递归遍历二叉树
+//先序、中序、后序、层次非递归遍历二叉树
 function TreeNode(val) {
   this.val = val;
   this.left = null;
@@ -71,6 +71,37 @@ var postorderTraversal = function(root) {
                 stack.push(null);
             }
         }
+    }
+    return node;
+};
+
+//层次遍历 同层元素为一个列表如 [[3],[9,20],[15,7]]
+var levelOrder = function(root) {
+    var queue = [], node = [], temp = [],current, current_node, current_level;
+    var num = 0, level = 1;
+    if(root){
+        queue.push([level,root]);
+        num++;
+    }
+    while(num){
+        temp = [];
+        while(num && queue[0][0] == level){
+            current = queue.shift();
+            current_level = current[0];
+            current_node = current[1];
+            num--;
+            temp.push(current_node.val);        
+            if(current_node.left!=null){
+                queue.push([current_level+1, current_node.left]);
+                num++;
+            }
+            if(current_node.right!=null){
+                queue.push([current_level+1, current_node.right]);
+                num++;
+            }
+        }
+        level++;
+        node.push(temp);
     }
     return node;
 };
