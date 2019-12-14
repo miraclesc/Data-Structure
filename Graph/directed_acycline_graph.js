46fcbf91ccfb94a9fba513a6d0dc90eef81ea4f4
Shi Chong <xmu_marvel@qq.com>
2019-12-14 16:42:31 +0800

    Update directed_acycline_graph.js

--- Graph/directed_acycline_graph.js
+++ Graph/directed_acycline_graph.js
@@ -1,14 +1,18 @@
 /*
-现在你总共有 n 门课需要选，记为 0 到 n-1。
-在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们: [0,1]
-给定课程总量以及它们的先决条件，判断是否可能完成所有课程的学习？
-示例 1:
-输入: 5,[[1,0],[2,1],[3,1],[0,3],[3,4]]
-输出: false
-解释: 3->1->0->3为环 不可能完成。
+排课表
+现在你总共有 n 门课需要选，记为 0 到 n-1。
+在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们: [0,1]
+给定课程总量以及它们的先决条件，返回你为了学完所有课程所安排的学习顺序。
+可能会有多个正确的顺序，你只要返回一种就可以了。如果不可能完成所有课程，返回一个空数组。
+
+示例 2:
+输入: 4, [[1,0],[2,0],[3,1],[3,2]]
+输出: [0,1,2,3] or [0,2,1,3]
+解释: 总共有 4 门课程。要学习课程 3，你应该先完成课程 1 和课程 2。并且课程 1 和课程 2 都应该排在课程 0 之后。
+     因此，一个正确的课程顺序是 [0,1,2,3] 。另一个正确的排序是 [0,2,1,3] 。
 
 来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/course-schedule
+链接：https://leetcode-cn.com/problems/course-schedule-ii
 */
 
 /**
@@ -16,29 +20,29 @@
  * @param {number[][]} prerequisites
  * @return {boolean}
  */
-var canFinish = function(numCourses, prerequisites) {
-    //先用邻接表(对象)表示图的结构 数组存储入度 用拓扑排序的方法验证是否有环
+var findOrder = function(numCourses, prerequisites) {
+    //先用邻接表(对象)表示图的结构 数组存储入度 用拓扑排序的方法填入课程顺序
     var adjacency = {};
     var indegree = new Array(numCourses);
-    var flag=1, num=0;
+    var flag=1,course = [];
     for(var i = 0; i < numCourses; i++){
         adjacency[i] = [];
         indegree[i] = 0;
     }
     for(var i = 0; i < prerequisites.length; i++){
-        adjacency[prerequisites[i][0]].push(prerequisites[i][1]);
-        indegree[prerequisites[i][1]]++; 
+        adjacency[prerequisites[i][1]].push(prerequisites[i][0]);
+        indegree[prerequisites[i][0]]++; 
     }
     
-    while(num < numCourses){
+    while(course.length < numCourses){
         flag = 1;
         for(var i = 0; i < numCourses; i++){
             if(indegree[i] == 0){
                 for(var j = 0; j<adjacency[i].length; j++){
                     indegree[adjacency[i][j]]--;
                 }
+                course.push(i);
                 indegree[i] = -1;
-                num++;
                 flag = 0;
             }
         }
@@ -46,9 +50,9 @@ var canFinish = function(numCourses, prerequisites) {
             break;
         }
     }
-    if(num == numCourses){
-        return true;
+    if(course.length == numCourses){
+        return course;
     }else{
-        return false;
+        return [];
     }
 };
